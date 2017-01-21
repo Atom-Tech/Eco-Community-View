3b0c862f97c383f8e79bc971fcbcec8757124c74
thiviscar <thiviscar@gmail.com>
2017-01-21 09:36:02 -0200

    Inserindo pacote

---
+++ Assets/scripts/RotaçãoPalheta.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaçãoPalheta : MonoBehaviour {
+    public float rotaçãoP = -40f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f,0f,rotaçãoP)*Time.deltaTime);
+	}
+}
